a0b72f7e89c2a0d740b367c5499a2c9fba6bde9f
JoseDLopez <josed-lopez@hotmail.com>
2016-11-21 19:34:46 -0300

    Agregamos el modelo de playlist y lo asociamos a usuarios y canciones, tambien asociamos generos a canciones. Creamos las rutas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121222046) do
+ActiveRecord::Schema.define(version: 20161121223202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20161121222046) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "song_id"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_id"], name: "index_playlists_on_song_id", using: :btree
+    t.index ["user_id"], name: "index_playlists_on_user_id", using: :btree
   end
 
   create_table "songs", force: :cascade do |t|
@@ -30,5 +40,25 @@ ActiveRecord::Schema.define(version: 20161121222046) do
     t.index ["genre_id"], name: "index_songs_on_genre_id", using: :btree
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "playlists", "songs"
+  add_foreign_key "playlists", "users"
   add_foreign_key "songs", "genres"
 end
